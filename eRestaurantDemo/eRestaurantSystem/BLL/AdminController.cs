c6919049a0bd0da2d3b16bb70bc99f73165e4e3b
Shuang Wu <swu18@studentmail.nait.ca>
2015-10-14 20:27:15 -0600

    Create Update and insert and delete event
    Create Update and insert and delete event

--- eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -21,31 +21,32 @@ namespace eRestaurantSystem.BLL
     [DataObject]
     public class AdminController
     {
+
+        #region Queries
         [DataObjectMethod(DataObjectMethodType.Select, false)]
         public List<SpecialEvent> SpecialEvents_List()
-
-        { 
-          //connect to our DbContext class in the DAL
-          //create an instance of the class
-          //constructor => connection name
-          // we will use a transaxtion to hold our query 
+        {
+            //connect to our DbContext class in the DAL
+            //create an instance of the class
+            //constructor => connection name
+            // we will use a transaxtion to hold our query 
             using (var context = new eRestaurantContext()) //context is variable name , new is instance name
-               { 
-               
-                  // option1: method syntax
-                   //return context.SpecialEvents.OrderBy(x => x.Description).ToList(); //.SpecialEvents is DbSet()
-                
+            {
+
+                // option1: method syntax
+                //return context.SpecialEvents.OrderBy(x => x.Description).ToList(); //.SpecialEvents is DbSet()
+
                 //option2:query syntax
-                   var results = from item in context.SpecialEvents
-                                 orderby item.Description
-                                 select item;
-                   return results.ToList();
+                var results = from item in context.SpecialEvents
+                              orderby item.Description
+                              select item;
+                return results.ToList();
 
-              
-               }
-          
 
-        
+            }
+
+
+
         }
 
         [DataObjectMethod(DataObjectMethodType.Select, false)]
@@ -54,85 +55,134 @@ namespace eRestaurantSystem.BLL
             using (var context = new eRestaurantContext()) //context is variable name , new is instance name
             {
 
-               //option2:query syntax
+                //option2:query syntax
                 var results = from item in context.Reservations
                               where item.EventCode.Equals(eventcode)
-                              orderby item.CustomerName,item.ReservationDate
+                              orderby item.CustomerName, item.ReservationDate
                               select item;
                 return results.ToList();
             }
 
         }
-            [DataObjectMethod(DataObjectMethodType.Select,false)]
-            
-                   public List<ReservationsByDate> GetReservationByDate(String reservationdate)// since reservationbydate is created by our own, has to put into name space
-                   {
-                   
-                   using (var context = new eRestaurantContext())
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
 
-                       {
-                        //Linq is not very playful or cooperative with 
-                       //DateTime
-                       //extract the year, moth and day ourseleves out
-                       //of the passed parameter value
+        public List<ReservationsByDate> GetReservationByDate(String reservationdate)// since reservationbydate is created by our own, has to put into name space
+        {
 
-                       int theYear = (DateTime.Parse(reservationdate)).Year;
-                       int theMonth = (DateTime.Parse(reservationdate)).Month;
-                       int theDay = (DateTime.Parse(reservationdate)).Day;
+            using (var context = new eRestaurantContext())
+            {
+                //Linq is not very playful or cooperative with 
+                //DateTime
+                //extract the year, moth and day ourseleves out
+                //of the passed parameter value
 
-                       var results = from eventitem in context.SpecialEvents
-                                     orderby eventitem.Description
-                                     select new ReservationsByDate()//ReservationByDate is optional) a new instance for each specialevent row on the table 
-                                     {
-                                         Description = eventitem.Description,
-                                         Reservation = from row in eventitem.Reservations
-                                                       where row.ReservationDate.Year == theYear
-                                                       && row.ReservationDate.Month == theMonth
-                                                       && row.ReservationDate.Day == theDay
-                                                       select new ReservationDetail()// a new for each reservation of the particular specialevent 
-                                                       {
-                                                           CustomerName = row.CustomerName,
-                                                           ReservationDate = row.ReservationDate,
-                                                           NumberInParty = row.NumberInParty,
-                                                           ContactPhone = row.ContactPhone,
-                                                           ReservationStatus = row.ReservationStatus
-                                                       }
+                int theYear = (DateTime.Parse(reservationdate)).Year;
+                int theMonth = (DateTime.Parse(reservationdate)).Month;
+                int theDay = (DateTime.Parse(reservationdate)).Day;
 
-                                     };
-                        return results.ToList();
+                var results = from eventitem in context.SpecialEvents
+                              orderby eventitem.Description
+                              select new ReservationsByDate()//ReservationByDate is optional) a new instance for each specialevent row on the table 
+                              {
+                                  Description = eventitem.Description,
+                                  Reservation = from row in eventitem.Reservations
+                                                where row.ReservationDate.Year == theYear
+                                                && row.ReservationDate.Month == theMonth
+                                                && row.ReservationDate.Day == theDay
+                                                select new ReservationDetail()// a new for each reservation of the particular specialevent 
+                                                {
+                                                    CustomerName = row.CustomerName,
+                                                    ReservationDate = row.ReservationDate,
+                                                    NumberInParty = row.NumberInParty,
+                                                    ContactPhone = row.ContactPhone,
+                                                    ReservationStatus = row.ReservationStatus
+                                                }
 
-                   
-                       }
-                   
-                   
-                   }
-               [DataObjectMethod(DataObjectMethodType.Select, false)] 
-      public List<MenuCategoryItems> MenuCategoryItems_List() 
-      { 
-          using (var context = new eRestaurantContext()) 
-          { 
-              var result = from menuitem in context.MenuCategories 
-                           orderby menuitem.Description 
-                           select new MenuCategoryItems() 
-                           { 
-                               Description = menuitem.Description, 
-                               MenuItems = from row in menuitem.MenuItems 
-                                            select new MenuItem() 
-                                              { 
-                                                  Description = row.Description, 
-                                                  Price = row.CurrentPrice, 
-                                                  Calories = row.Calories, 
-                                                  Comment = row.Comment 
-                                              } 
-                           }; 
-              return result.ToList(); 
-          } 
-      }
+                              };
+                return results.ToList();
 
 
+            }
 
+
+        }
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<MenuCategoryItems> MenuCategoryItems_List()
+        {
+            using (var context = new eRestaurantContext())
+            {
+                var result = from menuitem in context.MenuCategories
+                             orderby menuitem.Description
+                             select new MenuCategoryItems()
+                             {
+                                 Description = menuitem.Description,
+                                 MenuItems = from row in menuitem.MenuItems
+                                             select new MenuItem()
+                                               {
+                                                   Description = row.Description,
+                                                   Price = row.CurrentPrice,
+                                                   Calories = row.Calories,
+                                                   Comment = row.Comment
+                                               }
+                             };
+                return result.ToList();
+            }
+        }
+
+        #endregion
+
+        #region Add, Update,Delete of CRUD for CQRS(Comment query responsibility )
+        [DataObjectMethod(DataObjectMethodType.Insert, false)]
+        public void SpecialEvent_Add(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+
+                // these methods are execute using an instance level item
+                // set up a instance pointer and initialize to null
+                SpecialEvent added = null;
+                // set up commanc to execute the add
+                added = context.SpecialEvents.Add(item);
+                //command is not executed until it it actually saved.
+                context.SaveChanges();
+            }
+        }
+
+
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void SpecialEvent_Update(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+
+                // indicate the updateing item instance 
+                //alter the Modified Status flag for this instance
+                context.Entry<SpecialEvent>(context.SpecialEvents.Attach(item)).State =
+               System.Data.Entity.EntityState.Modified;
+                //command is not executed until it it actually saved.
+                context.SaveChanges();
+            }
+        }
+
+
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
+        public void SpecialEvent_Delete(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+
+                // indicate the updateing item instance 
+                //alter the Modified Status flag for this instance
+                SpecialEvent existing = context.SpecialEvents.Find(item.EventCode);
+                // set up the command to execute the delete
+                context.SpecialEvents.Remove(existing);
+                //command is not executed until it it actually saved.
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
     }
 
-
-}
+}// eof namespace
 
